0b403da8ad0b5a8253bf5039980c48f320d7b167
cipherjai <jaiatmart@gmail.com>
2016-04-19 18:30:27 +0530

    Encryption:Implementation

--- src/Encryption.java
+++ src/Encryption.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class Encryption {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String []s = scan.nextLine().split(" ");
+
+
+
     }
 }
